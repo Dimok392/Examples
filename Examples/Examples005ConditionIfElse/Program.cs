4858959686314e208655fa403d1f6ac96bfeaa06
Пётр <dimok392@mail.ru>
2022-04-16 22:45:45 +0300

    приветствие Машы

--- Examples/Examples005ConditionIfElse/Program.cs
+++ Examples/Examples005ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("ВВедите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это Маша");
 }
